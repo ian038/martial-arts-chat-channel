bd3cb3a4cb10d545809ed8526249eb54e43665a3
ian038 <ianphua3@gmail.com>
2021-12-03 12:38:28 -0500

    Completed search channel functionality

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,28 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useChatContext } from 'stream-chat-react'
-
+import { ResultsDropdown } from './'
 import { SearchIcon } from '../assets'
 
 export default function ChannelSearch({ setToggleContainer }) {
+    const { client, setActiveChannel } = useChatContext()
     const [query, setQuery] = useState('')
     const [loading, setLoading] = useState(false)
+    const [teamChannels, setTeamChannels] = useState([])
+    const [directChannels, setDirectChannels] = useState([])
+
+    useEffect(() => {
+        if(!query) {
+            setTeamChannels([])
+            setDirectChannels([])
+        }
+    }, [query])
 
     const getChannels = async (text) => {
         try {
-            // const channelResponse = client.queryChannels({
-            //     type: 'team', 
-            //     name: { $autocomplete: text }, 
-            //     members: { $in: [client.userID]}
-            // });
-            // const userResponse = client.queryUsers({
-            //     id: { $ne: client.userID },
-            //     name: { $autocomplete: text }
-            // })
+            const channelResponse = client.queryChannels({
+                type: 'team', 
+                name: { $autocomplete: text }, 
+                members: { $in: [client.userID]}
+            });
+            const userResponse = client.queryUsers({
+                id: { $ne: client.userID },
+                name: { $autocomplete: text }
+            })
 
-            // const [channels, { users }] = await Promise.all([channelResponse, userResponse]);
+            const [channels, { users }] = await Promise.all([channelResponse, userResponse])
 
-            // if(channels.length) setTeamChannels(channels);
-            // if(users.length) setDirectChannels(users);
+            if(channels.length) setTeamChannels(channels)
+            if(users.length) setDirectChannels(users)
         } catch (error) {
             setQuery('')
         }
@@ -34,6 +44,11 @@ export default function ChannelSearch({ setToggleContainer }) {
         setLoading(true)
         setQuery(e.target.value)
         getChannels(e.target.value)
+    }
+
+    const setChannel = (channel) => {
+        setQuery('')
+        setActiveChannel(channel)
     }
 
     return (
@@ -50,7 +65,7 @@ export default function ChannelSearch({ setToggleContainer }) {
                     onChange={onSearch}
                 />
             </div>
-            {/* { query && (
+            { query && (
                 <ResultsDropdown 
                     teamChannels={teamChannels}
                     directChannels={directChannels}
@@ -59,7 +74,7 @@ export default function ChannelSearch({ setToggleContainer }) {
                     setQuery={setQuery}
                     setToggleContainer={setToggleContainer}
                 />
-            )} */}
+            )}
         </div>
     )
 }
